edbdc7e098449d753680a292d5c6daaa7bd2bc7a
l3g10n <ejise45@gmail.com>
2023-05-28 19:09:50 +0100

    first repo push

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,20 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/app/layout.tsx": [
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/app/css/style.css",
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\",\"display\":\"swap\"}],\"variableName\":\"inter\"}",
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Architects_Daughter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-architects-daughter\",\"weight\":\"400\",\"display\":\"swap\"}],\"variableName\":\"architects_daughter\"}"
+    ],
+    "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/app/(default)/layout.tsx": [
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/node_modules/aos/dist/aos.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/app/(default)/page": [
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/app/css/style.css",
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\",\"display\":\"swap\"}],\"variableName\":\"inter\"}",
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Architects_Daughter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-architects-daughter\",\"weight\":\"400\",\"display\":\"swap\"}],\"variableName\":\"architects_daughter\"}",
+      "/Users/l3j10n/Documents/Projects/nurvid/web/frontend/CompanyPage-Frontend/node_modules/aos/dist/aos.css"
+    ]
+  }
+}
